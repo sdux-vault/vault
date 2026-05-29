cf3bc3846858a64d2010c7d6f803a542db930b24
SDuX Vault <sduxvault@gmail.com>
2026-05-29 18:26:12 -0500

    chore(shared): release version bump

--- libs/shared/src/lib/version/version.register.ts
+++ libs/shared/src/lib/version/version.register.ts
@@ -4,6 +4,6 @@ import { registerVersion } from '../utils/version/version.register';
 const SDUX_PACKAGE = '@sdux-vault/shared';
 
 /** Current package version string. */
-const SDUX_VERSION = '0.9.0';
+const SDUX_VERSION = '0.9.1';
 
 registerVersion(SDUX_PACKAGE, SDUX_VERSION);
