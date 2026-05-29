e61548e8bcaf070846cce6615ddf98f089fc0cfe
SDuX Vault <sduxvault@gmail.com>
2026-05-29 18:28:53 -0500

    chore(devtools): release version bump

--- libs/devtools/tooling/src/lib/version/version.register.ts
+++ libs/devtools/tooling/src/lib/version/version.register.ts
@@ -3,6 +3,6 @@ import { registerVersion } from '@sdux-vault/shared';
 /** Package name used for devtools version registration. */
 const SDUX_PACKAGE = '@sdux-vault/devtools';
 /** Current version of the devtools package. */
-const SDUX_VERSION = '0.9.0';
+const SDUX_VERSION = '0.9.1';
 
 registerVersion(SDUX_PACKAGE, SDUX_VERSION);
