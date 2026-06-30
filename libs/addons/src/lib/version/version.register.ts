3df84c0c2080d54f17ddcd4a66aedd271918d511
SDuX Vault <sduxvault@gmail.com>
2026-06-30 13:08:55 -0500

    chore(addons): release version bump (#41)

--- libs/addons/src/lib/version/version.register.ts
+++ libs/addons/src/lib/version/version.register.ts
@@ -3,6 +3,6 @@ import { registerVersion } from '@sdux-vault/shared';
 /** Package name used for addons version registration. */
 const SDUX_PACKAGE = '@sdux-vault/addons';
 /** Current version of the addons package. */
-const SDUX_VERSION = '1.0.1';
+const SDUX_VERSION = '1.0.2';
 
 registerVersion(SDUX_PACKAGE, SDUX_VERSION);
