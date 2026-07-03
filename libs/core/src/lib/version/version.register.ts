3954f06062011f6aab538000a416dc15f8a046f7
SDuX Vault <sduxvault@gmail.com>
2026-07-03 09:21:47 -0500

    chore(core): release version bump

--- libs/core/src/lib/version/version.register.ts
+++ libs/core/src/lib/version/version.register.ts
@@ -3,6 +3,6 @@ import { registerVersion } from '@sdux-vault/shared';
 /** Package name used for core version registration. */
 const SDUX_PACKAGE = '@sdux-vault/core';
 /** Current version of the core package. */
-const SDUX_VERSION = '1.0.2';
+const SDUX_VERSION = '1.0.3';
 
 registerVersion(SDUX_PACKAGE, SDUX_VERSION);
