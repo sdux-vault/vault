f925fcb1685a428f83c49200e516f29654540385
SDuX Vault <sduxvault@gmail.com>
2026-07-15 13:50:47 -0500

    chore(react): 1.0.3 release version bump

--- libs/core-extensions/react/src/lib/version/version.register.ts
+++ libs/core-extensions/react/src/lib/version/version.register.ts
@@ -3,6 +3,6 @@ import { registerVersion } from '@sdux-vault/shared';
 /** Package name used for React core-extensions version registration. */
 const SDUX_PACKAGE = '@sdux-vault/react';
 /** Current version of the React core-extensions package. */
-const SDUX_VERSION = '1.0.2';
+const SDUX_VERSION = '1.0.3';
 
 registerVersion(SDUX_PACKAGE, SDUX_VERSION);
