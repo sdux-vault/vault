841e008d777b5e0135a0439ea18be12dd91b2a46
SDuX Vault <sduxvault@gmail.com>
2026-07-27 16:50:24 -0500

    Updates to css

--- apps/docs-app/app/splash-page/dev/shapes/framework-comparison-implementation.shape.ts
+++ apps/docs-app/app/splash-page/dev/shapes/framework-comparison-implementation.shape.ts
@@ -1,8 +1,10 @@
 import { FrameworkComparisonSourceFileShape } from './framework-comparison-source-file.shape';
+import { FrameworkComparisonLineMetadataShape } from './framework-comparison-line-metadata.shape';
 
 export type FrameworkComparisonImplementationShape = {
   readonly frameworkLabel: string;
   readonly libraryLabel: string;
   readonly files: readonly FrameworkComparisonSourceFileShape[];
+  readonly metadata?: FrameworkComparisonLineMetadataShape;
   readonly usesSduxBrandName?: boolean;
 };
